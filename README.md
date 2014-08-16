90f1b4f24e9d8faeb5ff8797818873eeb9ab5002
Atticus White <contact@atticuswhite.com>
2014-08-16 13:03:52 -0400

    working version

---
+++ README.md
@@ -0,0 +1,33 @@
+#gulp-ng-config
+It's often useful to generate a file of constants, usually as environment variables, for your Angular apps.
+This Gulp plugin will allow you to provide an object of properties and will generate an Angular module of constants.
+
+### How it works
+It's pretty simple:
+`gulpNgConfig(outputFileName, moduleName, constantsObject)`
+
+
+
+### Example Usage
+```javascript
+gulp.task('test', function () {
+  gulp.src('testfile.json')
+  .pipe(gulpNgConfig('output.js', {
+    moduleName: 'MyModule',
+    constants: {
+      string: 'my string',
+      integer: 12345,
+      object: {one: 2, three: ['four']},
+      array: ['one', 2, {three: 'four'}, [5, 'six']]
+    }
+  }));
+});
+```
+### Example Output
+```html
+angular.module('MyModule')
+.constant('string', "my string")
+.constant('integer', 12345)
+.constant('object', {"one":2,"three":["four"]})
+.constant('array', ["one",2,{"three":"four"},[5,"six"]]);
+```
--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-var through = require('through');
+var through = require('through2'),
+    combine = require('stream-combiner'),
+    _ = require('lodash'),
+    fs = require('fs');
 
-module.exports = function (filename) {
+module.exports = function (fileName, moduleName, constantsObject) {
+  if (!fileName) throw new PluginError('gulp-ng-config', 'Missing fileName option for gulp-ng-config');
+  if (!moduleName) throw new PluginError('gulp-ng-config', 'Missing required moduleName option for gulp-ng-config');
+  if (!_.isObject(constantsObject)) throw new PluginError('gulp-ng-config', 'Missing required constants option for gulp-ng-config');
 
-  function bufferContents (file) {
-    if (file.isNull()) return;
-    if (file.isStream()) return new PluginError('gulp-ng-config', 'Streaming not suported');
+  var outputFileStream = fs.createWriteStream(fileName),
+      templateFile = fs.readFileSync(__dirname + '/template.html', 'utf8'),
+      constants = [],
+      jsonReader,
+      templater;
 
-  }
+  _.each(constantsObject, function (value, key) {
+    value = JSON.stringify(value);
+    constants.push({
+      name: key,
+      value: value
+    });
+  });
 
-  function endStream () {
+  jsonReader = through.obj(function (file, encoding, callback) {
+    this.push(file.contents.toString('utf8'));
+    callback();
+  });
 
-  }
+  templater = through(function (chunk, encoding, callback) {
+    v = _.template(templateFile, {moduleName: moduleName, constants: constants});
+    this.push(v);
+    callback();
+  });
 
-  return through(bufferContents, endStream);
+
+  return combine(jsonReader, templater, outputFileStream);
 };

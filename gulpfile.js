9616903bd8f98b564a1f2c7348d8dd90cb6b46bc
Atticus White <contact@atticuswhite.com>
2015-11-08 17:51:15 -0500

    ignores

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
     jscs = require('gulp-jscs'),
     jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+    stylish = require('jshint-stylish'),
+    istanbul = require('gulp-istanbul');
 
 gulp.task('lint', function () {
   gulp.src(['gulp-ng-config.js', 'test/stream.js'])
@@ -15,9 +16,16 @@ gulp.task('style', function () {
   .pipe(jscs());
 });
 
-gulp.task('unittest', function () {
+gulp.task('pre-test', function () {
+  return gulp.src('gulp-ng-config.js')
+  .pipe(istanbul())
+  .pipe(istanbul.hookRequire());
+})
+
+gulp.task('unittest', ['pre-test'], function () {
   gulp.src('test/stream.js')
-  .pipe(mocha({reporter: 'spec'}));
+  .pipe(mocha({reporter: 'spec'}))
+  .pipe(istanbul.writeReports());
 });
 
 gulp.task('test', ['lint', 'style', 'unittest']);

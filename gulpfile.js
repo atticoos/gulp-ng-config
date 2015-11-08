c85a58135a914cee6f2f1811d1872b466c3a0678
Atticus White <contact@atticuswhite.com>
2015-11-08 17:12:09 -0500

    return streams

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,18 @@ var gulp = require('gulp'),
     stylish = require('jshint-stylish');
 
 gulp.task('lint', function () {
-  gulp.src(['gulp-ng-config.js', 'test/stream.js'])
+  return gulp.src(['gulp-ng-config.js', 'test/stream.js'])
   .pipe(jshint('.jshintrc'))
   .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('style', function () {
-  gulp.src(['gulp-ng-config.js', 'test/stream.js'])
+  return gulp.src(['gulp-ng-config.js', 'test/stream.js'])
   .pipe(jscs());
 });
 
 gulp.task('unittest', function () {
-  gulp.src('test/stream.js')
+  return gulp.src('test/stream.js')
   .pipe(mocha({reporter: 'spec'}));
 });
 
